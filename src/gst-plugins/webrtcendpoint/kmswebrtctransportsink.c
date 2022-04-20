338fbffbe25d477940de7f19f87f015081e5b32c
Saul Pablo Labajo Izquierdo <slabajo@naevatec.com>
2022-04-20 20:19:11 +0200

    Fix for quick DTLS connection

--- src/gst-plugins/webrtcendpoint/kmswebrtctransportsink.c
+++ src/gst-plugins/webrtcendpoint/kmswebrtctransportsink.c
@@ -31,15 +31,24 @@ G_DEFINE_TYPE (KmsWebrtcTransportSink, kms_webrtc_transport_sink, GST_TYPE_BIN);
 
 #define FUNNEL_NAME "funnel"
 #define SRTPENC_NAME "srtp-encoder"
+#define DTLS_ENCODER_NAME "dtls-encoder"
 
 static void
 kms_webrtc_transport_sink_init (KmsWebrtcTransportSink * self)
 {
+  GstElement *dtls_encoder;
+
   self->dtlssrtpenc = gst_element_factory_make ("dtlssrtpenc", NULL);
+  dtls_encoder = gst_bin_get_by_name (GST_BIN(self->dtlssrtpenc), DTLS_ENCODER_NAME);
+  if (dtls_encoder != NULL) {
+    gst_element_set_locked_state (dtls_encoder, TRUE);
+  } else  {
+    GST_WARNING ("Cannot get DTLS encoder with name %s", DTLS_ENCODER_NAME);
+  }
 }
 
 void
-kms_webrtc_transport_sink_connect_elements (KmsWebrtcTransportSink * self)
+kms_webrtc_transport_sink_connect_elements (KmsWebrtcTransportSink *self)
 {
   GstElement *funnel, *srtpenc;
 
@@ -62,6 +71,7 @@ kms_webrtc_transport_sink_connect_elements (KmsWebrtcTransportSink * self)
   } else {
     GST_WARNING ("Cannot get srtpenc with name %s", SRTPENC_NAME);
   }
+
 }
 
 void
@@ -78,6 +88,18 @@ kms_webrtc_transport_sink_configure_default (KmsWebrtcTransportSink * self,
 }
 
 void
+kms_webrtc_transport_sink_set_dtls_is_client_default (KmsWebrtcTransportSink * self,
+    gboolean is_client)
+{
+  g_object_set (G_OBJECT (self->dtlssrtpenc), "is-client", is_client, NULL);
+  if (is_client) {
+    GST_DEBUG_OBJECT(self, "Set DTLS client");
+  } else {
+    GST_DEBUG_OBJECT(self, "Set DTLS server");
+  }
+}
+
+void
 kms_webrtc_transport_sink_configure (KmsWebrtcTransportSink * self,
     KmsIceBaseAgent * agent, const char *stream_id, guint component_id)
 {
@@ -87,12 +109,23 @@ kms_webrtc_transport_sink_configure (KmsWebrtcTransportSink * self,
   klass->configure (self, agent, stream_id, component_id);
 }
 
+void
+kms_webrtc_transport_sink_set_dtls_is_client (KmsWebrtcTransportSink * self,
+    gboolean is_client)
+{
+  KmsWebrtcTransportSinkClass *klass =
+      KMS_WEBRTC_TRANSPORT_SINK_CLASS (G_OBJECT_GET_CLASS (self));
+
+  klass->set_dtls_is_client (self, is_client);
+}
+
 static void
 kms_webrtc_transport_sink_class_init (KmsWebrtcTransportSinkClass * klass)
 {
   GstElementClass *gstelement_class = GST_ELEMENT_CLASS (klass);
 
   klass->configure = kms_webrtc_transport_sink_configure_default;
+  klass->set_dtls_is_client = kms_webrtc_transport_sink_set_dtls_is_client_default;
 
   GST_DEBUG_CATEGORY_INIT (GST_CAT_DEFAULT, GST_DEFAULT_NAME, 0,
       GST_DEFAULT_NAME);
@@ -103,6 +136,22 @@ kms_webrtc_transport_sink_class_init (KmsWebrtcTransportSinkClass * klass)
       "Base bin to manage WebRTC transport sink elements.",
       "Miguel París Díaz <mparisdiaz@gmail.com>");
 }
+
+void 
+kms_webrtc_transport_sink_start_dtls (KmsWebrtcTransportSink * self)
+{
+  GstElement *dtls_encoder;
+
+  dtls_encoder = gst_bin_get_by_name (GST_BIN(self->dtlssrtpenc), DTLS_ENCODER_NAME);
+  if (dtls_encoder != NULL) {
+    gst_element_set_locked_state (dtls_encoder, FALSE);
+    gst_element_sync_state_with_parent (dtls_encoder);
+    GST_DEBUG_OBJECT(self, "Starting DTLS");
+  } else  {
+    GST_WARNING ("Cannot get DTLS encoder with name %s", DTLS_ENCODER_NAME);
+  }
+}
+
 
 KmsWebrtcTransportSink *
 kms_webrtc_transport_sink_new ()
